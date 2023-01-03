bf987c42fbe57a9fc53e7f87391ff0a13cc9eb0d
Hildong <philip.hilding@hotmail.com>
2023-01-03 18:47:57 +0100

    Added some styling for navbar

--- ecommerce-frontend/src/Components/header.js
+++ ecommerce-frontend/src/Components/header.js
@@ -5,7 +5,16 @@ import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import '../Styling/header.css'
 
 function Header() {
     const [anchorEl, setAnchorEl] = useState();
@@ -24,15 +33,15 @@ function Header() {
   
 
     return (
-      <div className="App">
-        <div>
-            <section>
+      <div className="navbar">
+        <div className='nav-links-and-site-btns'>
+            <section className='nav-links'>
                 <li>About</li>
                 <li>Contact</li>
                 <li>Help</li>
                 <li>FAQs</li>
             </section>
-            <section>
+            <section className='currency-account-btn'>
                 <Button
                         id="basic-button"
                         aria-controls={openAccountBtn ? 'basic-menu' : undefined}
@@ -79,24 +88,49 @@ function Header() {
                 </Menu>
             </section>
         </div>
-        <div>
+        <div className='logo-and-searchbar'>
             <div class="logo">
                 Shopshop
             </div>
-            <div>
-                <FormControl fullWidth sx={{ m: 1 }}>
+            <div className='search-bar'>
+                <FormControl className='hej' fullWidth sx={{ m: 1 }}>
                     <TextField
+                        size='small'
                         id="outlined-adornment-amount"
                         startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                        label="Amount"
+                        label="Search"
                     />
                 </FormControl>
+                <Button><SearchIcon /></Button>
+            </div>
+            <div className='customer-service-info'>
+                <h4>Customer service</h4>
+                <p>+012 345 6789</p>
             </div>
         </div>
-        <nav>
-
+        <nav className='category-favorites-and-cart'>
+            <section className='categories-div'>
+                <Accordion className='categories'>
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography className='category-text'><MenuIcon /> Categories</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </section>
+            <div className='favorites-cart'>
+                <p className='favorites'><FavoriteIcon /><p>1</p></p>
+                <p className='cart'><ShoppingCartIcon /><p>1</p></p>
+            </div>
         </nav>
-        <p>Landing page</p>
       </div>
     );
   }
